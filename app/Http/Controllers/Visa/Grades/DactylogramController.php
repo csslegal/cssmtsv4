3224b885c2b6ae1c2e1de3467e13839a399e3d4e
Rplus Media <rplusmediaweb@gmail.com>
2021-10-14 16:59:15 +0300

    Randevu ertelenmesi işlemi tamamlandı

--- app/Http/Controllers/Visa/Grades/DactylogramController.php
+++ app/Http/Controllers/Visa/Grades/DactylogramController.php
@@ -44,45 +44,84 @@ class DactylogramController extends Controller
      */
     public function store($id, $visa_file_id, Request $request)
     {
-        $visaFileGradesId = DB::table('visa_files')
-            ->select(['visa_file_grades_id'])
-            ->where('id', '=', $visa_file_id)
-            ->first();
 
-        $visaFileGradesName = new VisaFileGradesName(
-            $visaFileGradesId->visa_file_grades_id
-        );
 
-        DB::table('visa_file_logs')->insert([
-            'visa_file_id' => $visa_file_id,
-            'user_id' => $request->session()->get('userId'),
-            'subject' => $visaFileGradesName->getName(),
-            'content' => 'Parmak izi verildi',
-            'created_at' => date('Y-m-d H:i:s'),
-        ]);
+        if ($request->has('tamam')) {
+            $visaFileGradesId = DB::table('visa_files')
+                ->select(['visa_file_grades_id'])
+                ->where('id', '=', $visa_file_id)
+                ->first();
 
-        $whichGrades = new VisaFileWhichGrades();
-        $nextGrades = $whichGrades->nextGrades($visa_file_id);
+            $visaFileGradesName = new VisaFileGradesName(
+                $visaFileGradesId->visa_file_grades_id
+            );
 
-        if ($nextGrades != null) {
+            DB::table('visa_file_logs')->insert([
+                'visa_file_id' => $visa_file_id,
+                'user_id' => $request->session()->get('userId'),
+                'subject' => $visaFileGradesName->getName(),
+                'content' => 'Parmak izi verildi',
+                'created_at' => date('Y-m-d H:i:s'),
+            ]);
+
+            $whichGrades = new VisaFileWhichGrades();
+            $nextGrades = $whichGrades->nextGrades($visa_file_id);
+
+            if ($nextGrades != null) {
+
+                if (DB::table('visa_files')
+                    ->where("id", "=", $visa_file_id)
+                    ->update(['visa_file_grades_id' => $nextGrades])
+                ) {
+                    $request->session()
+                        ->flash('mesajSuccess', 'Kayıt başarıyla yapıldı');
+
+                    return redirect('/musteri/' . $id . '/vize');
+                } else {
+                    $request->session()
+                        ->flash('mesajDanger', 'Güncelleme sıralasında sorun oluştu');
+                    return redirect('/musteri/' . $id . '/vize');
+                }
+            } else {
+                $request->session()
+                    ->flash('mesajDanger', 'Sonraki aşama bulunamadı');
+                return redirect('/musteri/' . $id . '/vize');
+            }
+        }
+        if ($request->has('ertele')) {
+            $visaFileGradesId = DB::table('visa_files')
+                ->select(['visa_file_grades_id'])
+                ->where('id', '=', $visa_file_id)
+                ->first();
+
+            $visaFileGradesName = new VisaFileGradesName(
+                $visaFileGradesId->visa_file_grades_id
+            );
+
+            DB::table('visa_file_logs')->insert([
+                'visa_file_id' => $visa_file_id,
+                'user_id' => $request->session()->get('userId'),
+                'subject' => $visaFileGradesName->getName(),
+                'content' => 'Randevu ertelendi',
+                'created_at' => date('Y-m-d H:i:s'),
+            ]);
 
             if (DB::table('visa_files')
                 ->where("id", "=", $visa_file_id)
-                ->update(['visa_file_grades_id' => $nextGrades])
+                ->update(['visa_file_grades_id' => env('VISA_APPOINTMENT_PUTOFF_GRADES_ID')])
             ) {
                 $request->session()
                     ->flash('mesajSuccess', 'Kayıt başarıyla yapıldı');
 
-                return redirect('/musteri/' . $id . '/vize/');
+                return redirect('/musteri/' . $id . '/vize');
             } else {
                 $request->session()
                     ->flash('mesajDanger', 'Güncelleme sıralasında sorun oluştu');
-                return redirect('/musteri/' . $id . '/vize/' . $visa_file_id . '/parmak-izi');
+                return redirect('/musteri/' . $id . '/vize');
             }
-        } else {
-            $request->session()
-                ->flash('mesajDanger', 'Sonraki aşama bulunamadı');
-            return redirect('/musteri/' . $id . '/vize');
+        }
+        if ($request->has('iptal')) {
+            dd('iptal');
         }
     }
 
--- resources/views/customer/visa/grades/dactylogram.blade.php
+++ resources/views/customer/visa/grades/dactylogram.blade.php
@@ -18,46 +18,45 @@
         <div class="card-header bg-primary text-white">Bütün İşlemler</div>
         <div class="card-body scroll">
 
-            <div class="row">
-                <div class="col-lg-4 col-md-6 col-sm-6 ">
-                    <div class="card border-success mb-2">
-                        <div class="card-body">
-                            <h5 class="card-title">Parmak izi verildi mi?</h5>
-                            <p>&nbsp;</p>
-                            <form action="" method="POST">
+            <form action="" method="POST">
+                <div class="row">
+                    <div class="col-lg-4 col-md-6 col-sm-6 ">
+                        <div class="card border-success mb-2">
+                            <div class="card-body">
+                                <h5 class="card-title">Parmak izi verildi mi?</h5>
+                                <p>&nbsp;</p>
                                 @csrf
-                                <button name="tamam" type="submit" class="confirm btn btn-success float-end text-white "
+                                <button type="submit" name="tamam" value="tamam"
+                                    class="confirm btn btn-success float-end text-white "
                                     data-title="Devam edilsin mi?">Tamamla</button>
-                            </form>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="col-lg-4 col-md-6 col-sm-6 ">
-                    <div class="card border-info mb-2">
-                        <div class="card-body">
-                            <h5 class="card-title">Randevu ertelendi mi?</h5>
-                            <p>&nbsp;</p>
-                            <form action="" method="POST">
+                    <div class="col-lg-4 col-md-6 col-sm-6 ">
+                        <div class="card border-info mb-2">
+                            <div class="card-body">
+                                <h5 class="card-title">Randevu ertelendi mi?</h5>
+                                <p>&nbsp;</p>
                                 @csrf
-                                <button name="ertele" type="submit" class="btn btn-info float-end text-white confirm"
-                                    data-title="Ertelensin mi?">Ertele</button>
-                            </form>
+                                <button type="submit" class="btn btn-info float-end text-white confirm" name="ertele"
+                                    value="ertele" data-title="Ertelensin mi?">Ertele</button>
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="col-lg-4 col-md-6 col-sm-6 ">
-                    <div class="card border-danger mb-2">
-                        <div class="card-body">
-                            <h5 class="card-title">Randevu iptal mi?</h5>
-                            <p>&nbsp;</p>
-                            <form action="" method="POST">
+                    <div class="col-lg-4 col-md-6 col-sm-6 ">
+                        <div class="card border-danger mb-2">
+                            <div class="card-body">
+                                <h5 class="card-title">Randevu iptal mi?</h5>
+                                <p>&nbsp;</p>
                                 @csrf
-                                <button name="iptal" type="submit" class="btn btn-danger float-end text-white confirm"
-                                    data-title="İptal edilsin mi?">İptal Et</button>
-                            </form>
+                                <button name="iptal" value="iptal" type="submit"
+                                    class="btn btn-danger float-end text-white confirm" data-title="İptal edilsin mi?">İptal
+                                    Et</button>
+
+                            </div>
                         </div>
                     </div>
-                </div>
-            </div>
+            </form>
         </div>
-    @endsection
+    </div>
+@endsection
