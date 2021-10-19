e88a441581a9d87612c340fc092d05bbb13eef7b
Rplus Media <rplusmediaweb@gmail.com>
2021-10-19 17:06:45 +0300

    Uzman yeniden randevu alma işlemi tamamlandı

--- app/Http/Controllers/User/IndexController.php
+++ app/Http/Controllers/User/IndexController.php
@@ -67,6 +67,7 @@ class IndexController extends Controller
                     ->where('visa_files.active', '=', 1)
                     ->where('visa_files.expert_id', '=', $request->session()->get('userId'))
                     ->where('visa_files.visa_file_grades_id', '=', env('VISA_APPOINTMENT_GRADES_ID'))
+                    ->orWhere('visa_files.visa_file_grades_id', '=', env('VISA_APPOINTMENT_PUTOFF_GRADES_ID'))
                     ->get();
 
                 return view('user.uzman.index')->with([
