38c4a5bc0c424e8bf9b3049c95f9c0b03569bd95
Rplus Media <rplusmediaweb@gmail.com>
2021-10-19 17:09:17 +0300

    Yeniden randevu güncelleme tamamlandı

--- app/Http/Controllers/Visa/Grades/AppointmentCompletedController.php
+++ app/Http/Controllers/Visa/Grades/AppointmentCompletedController.php
@@ -38,67 +38,73 @@ class AppointmentCompletedController extends Controller
 
         $request->validate($validatorStringArray);
 
+        $visaFileGradesId = DB::table('visa_files')
+            ->select(['visa_file_grades_id'])
+            ->where('id', '=', $visa_file_id)
+            ->first();
 
-        $customerAppointmentCount = DB::table('visa_appointments')
-            ->where('visa_file_id', '=', $visa_file_id)
-            ->get()->count();
+        $visaFileGradesName = new VisaFileGradesName(
+            $visaFileGradesId->visa_file_grades_id
+        );
+        $whichGrades = new VisaFileWhichGrades();
+        $nextGrades = $whichGrades->nextGrades($visa_file_id);
+        if ($nextGrades != null) {
+            $customerAppointmentCount = DB::table('visa_appointments')
+                ->where('visa_file_id', '=', $visa_file_id)
+                ->get()->count();
 
-        if ($customerAppointmentCount == 0) {
+            if ($customerAppointmentCount == 0) {
 
-            $visaFileGradesId = DB::table('visa_files')
-                ->select(['visa_file_grades_id'])
-                ->where('id', '=', $visa_file_id)
-                ->first();
-
-            $visaFileGradesName = new VisaFileGradesName(
-                $visaFileGradesId->visa_file_grades_id
-            );
-
-            $whichGrades = new VisaFileWhichGrades();
-            $nextGrades = $whichGrades->nextGrades($visa_file_id);
-
-            if ($nextGrades != null) {
-
-                $insertDataArray = array(
-                    'visa_file_id' => $visa_file_id,
-                    'user_id' => $request->session()->get('userId'),
-                    'gwf' => $request->input('gwf'),
-                    'name' => $request->input('hesap_adi'),
-                    'password' => $request->input('sifre'),
-                    'date' => $request->input('tarih'),
-                    'time' => $request->input('saat'),
-                    'created_at' => date('Y-m-d H:i:s'),
-                );
-
-                if (DB::table('visa_appointments')->insert($insertDataArray)) {
-
-                    DB::table('visa_files')
-                        ->where("id", "=", $visa_file_id)
-                        ->update(['visa_file_grades_id' => $nextGrades]);
-
-                    DB::table('visa_file_logs')->insert([
+                DB::table('visa_appointments')->insert(
+                    array(
                         'visa_file_id' => $visa_file_id,
                         'user_id' => $request->session()->get('userId'),
-                        'subject' => $visaFileGradesName->getName(),
-                        'content' => 'Randevu bilgisi kaydedildi',
+                        'gwf' => $request->input('gwf'),
+                        'name' => $request->input('hesap_adi'),
+                        'password' => $request->input('sifre'),
+                        'date' => $request->input('tarih'),
+                        'time' => $request->input('saat'),
                         'created_at' => date('Y-m-d H:i:s'),
-                    ]);
-
-                    $request->session()
-                        ->flash('mesajSuccess', 'Kayıt başarıyla yapıldı');
-                    return redirect('/musteri/' . $id . '/vize');
-                } else {
-                    $request->session()
-                        ->flash('mesajDanger', 'Kayıt sırasında sorun oluştu');
-                    return redirect('/musteri/' . $id . '/vize' );
-                }
+                    )
+                );
+                DB::table('visa_file_logs')->insert([
+                    'visa_file_id' => $visa_file_id,
+                    'user_id' => $request->session()->get('userId'),
+                    'subject' => $visaFileGradesName->getName(),
+                    'content' => 'Randevu bilgisi kaydedildi',
+                    'created_at' => date('Y-m-d H:i:s'),
+                ]);
             } else {
-                $request->session()
+                DB::table('visa_appointments')->where("visa_file_id", "=", $visa_file_id)
+                    ->update(
+                        array(
+                            'user_id' => $request->session()->get('userId'),
+                            'gwf' => $request->input('gwf'),
+                            'name' => $request->input('hesap_adi'),
+                            'password' => $request->input('sifre'),
+                            'date' => $request->input('tarih'),
+                            'time' => $request->input('saat'),
+                            'created_at' => date('Y-m-d H:i:s'),
+                        )
+                    );
+                DB::table('visa_file_logs')->insert([
+                    'visa_file_id' => $visa_file_id,
+                    'user_id' => $request->session()->get('userId'),
+                    'subject' => $visaFileGradesName->getName(),
+                    'content' => 'Randevu bilgisi güncellendi',
+                    'created_at' => date('Y-m-d H:i:s'),
+                ]);
+            }
+
+            DB::table('visa_files')->where("id", "=", $visa_file_id)
+                ->update(['visa_file_grades_id' => $nextGrades]);
+            $request->session()
+                ->flash('mesajSuccess', 'İşlem başarıyla tamamlandı');
+            return redirect('/musteri/' . $id . '/vize');
+        } else {
+            $request->session()
                 ->flash('mesajDanger', 'Sonraki aşama bulunamadı');
             return redirect('/musteri/' . $id . '/vize');
-            }
-        } else {
-            //form kaydını güncelleme burdan devam edecek
         }
     }
 }
