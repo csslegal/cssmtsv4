974d56502ce5da26a0162e4be4c904fcb0fb0dbe
Rplus Media <rplusmediaweb@gmail.com>
2021-10-23 12:53:37 +0300

    Kod düzenlendi

--- routes/web.php
+++ routes/web.php
@@ -146,58 +146,55 @@ Route::middleware(['sessionCheck'])->group(function () {
     });
 
     /**Yonetim yönlendirmeler*/
-    Route::group(
-        ['prefix' => 'yonetim', 'middleware' => 'managementCheck'],
-        function () {
+    Route::group(['prefix' => 'yonetim', 'middleware' => 'managementCheck'], function () {
 
-            Route::get('/', [ManagementIndexController::class, 'get_index']);
-            Route::get('mTBGI/{id}/onay', [ManagementIndexController::class, 'get_TBGI_onay']);
-            Route::get('mTBGI/{id}/geri-al', [ManagementIndexController::class, 'get_TBGI_gerial']);
+        Route::get('/', [ManagementIndexController::class, 'get_index']);
+        Route::get('mTBGI/{id}/onay', [ManagementIndexController::class, 'get_TBGI_onay']);
+        Route::get('mTBGI/{id}/geri-al', [ManagementIndexController::class, 'get_TBGI_gerial']);
 
-            Route::resource('duyuru', ManagementNoticeController::class);
-            Route::resource('profil', ManagementProfilController::class);
-            Route::resource('users', ManagementUsersController::class);
-            Route::resource('users-type', ManagementUsersTypeController::class);
-            Route::resource('users-access', ManagementUsersAccessController::class);
-            Route::resource('application-office', ManagementApplicationOfficeController::class);
-            Route::resource('appointment-office', ManagementAppointmentOfficeController::class);
-            Route::resource('language', ManagementLanguageController::class);
+        Route::resource('duyuru', ManagementNoticeController::class);
+        Route::resource('profil', ManagementProfilController::class);
+        Route::resource('users', ManagementUsersController::class);
+        Route::resource('users-type', ManagementUsersTypeController::class);
+        Route::resource('users-access', ManagementUsersAccessController::class);
+        Route::resource('application-office', ManagementApplicationOfficeController::class);
+        Route::resource('appointment-office', ManagementAppointmentOfficeController::class);
+        Route::resource('language', ManagementLanguageController::class);
 
-            /*** Yonetim ajax işlemleri*/
-            Route::group(['prefix' => 'ajax'], function () {
-                Route::post('duyuru', [ManagementAjaxController::class, 'post_duyuru_cek']);
-                Route::post('bilgi-emaili', [ManagementAjaxController::class, 'post_bilgi_emaili_cek']);
-                Route::post('evrak-emaili', [ManagementAjaxController::class, 'post_evrak_emaili_cek']);
-                Route::post('sirala', [ManagementAjaxController::class, 'post_sorting']);
-                Route::post('dosya-asama-erisim', [ManagementAjaxController::class, 'post_visa_file_grades_users_type']);
-            });
+        /*** Yonetim ajax işlemleri*/
+        Route::group(['prefix' => 'ajax'], function () {
+            Route::post('duyuru', [ManagementAjaxController::class, 'post_duyuru_cek']);
+            Route::post('bilgi-emaili', [ManagementAjaxController::class, 'post_bilgi_emaili_cek']);
+            Route::post('evrak-emaili', [ManagementAjaxController::class, 'post_evrak_emaili_cek']);
+            Route::post('sirala', [ManagementAjaxController::class, 'post_sorting']);
+            Route::post('dosya-asama-erisim', [ManagementAjaxController::class, 'post_visa_file_grades_users_type']);
+        });
 
-            /**Yonetim vize işlemleri*/
-            Route::group(['prefix' => 'vize'], function () {
+        /**Yonetim vize işlemleri*/
+        Route::group(['prefix' => 'vize'], function () {
 
-                Route::get('/', [ManagementVisaController::class, 'get_index']);
-                Route::get('danisman', [ManagementVisaController::class, 'get_danisman']);
-                Route::get('uzman', [ManagementVisaController::class, 'get_uzman']);
-                Route::get('muhasebe', [ManagementVisaController::class, 'get_muhasebe']);
-                Route::get('tercuman', [ManagementVisaController::class, 'get_tercuman']);
-                Route::get('koordinator', [ManagementVisaController::class, 'get_koordinator']);
-                Route::get('ofis-sorumlusu', [ManagementVisaController::class, 'get_ofis_sorumlusu']);
+            Route::get('/', [ManagementVisaController::class, 'get_index']);
+            Route::get('danisman', [ManagementVisaController::class, 'get_danisman']);
+            Route::get('uzman', [ManagementVisaController::class, 'get_uzman']);
+            Route::get('muhasebe', [ManagementVisaController::class, 'get_muhasebe']);
+            Route::get('tercuman', [ManagementVisaController::class, 'get_tercuman']);
+            Route::get('koordinator', [ManagementVisaController::class, 'get_koordinator']);
+            Route::get('ofis-sorumlusu', [ManagementVisaController::class, 'get_ofis_sorumlusu']);
 
-                Route::resource('vize-tipi', ManagementVisaTypesController::class);
-                Route::resource('vize-suresi', ManagementVisaValidityController::class);
-                Route::resource('alt-vize-tipi', ManagementVisaSubTypesController::class);
-                Route::resource('bilgi-emaili', ManagementVisaEmailsInformationController::class);
-                Route::resource('evrak-emaili', ManagementVisaEmailsDocumentListController::class);
-                Route::resource('dosya-asama', ManagementVisaFileGradesController::class);
-                Route::resource('dosya-asama-erisim', ManagementVisaFileGradesUsersTypeController::class);
-            });
+            Route::resource('vize-tipi', ManagementVisaTypesController::class);
+            Route::resource('vize-suresi', ManagementVisaValidityController::class);
+            Route::resource('alt-vize-tipi', ManagementVisaSubTypesController::class);
+            Route::resource('bilgi-emaili', ManagementVisaEmailsInformationController::class);
+            Route::resource('evrak-emaili', ManagementVisaEmailsDocumentListController::class);
+            Route::resource('dosya-asama', ManagementVisaFileGradesController::class);
+            Route::resource('dosya-asama-erisim', ManagementVisaFileGradesUsersTypeController::class);
+        });
 
-            /*****Url tespit İşlemleri */
-            Route::resource('url', UrlController::class);
-            Route::post('/url/ajax', [UrlController::class, 'get_ajax']);
-            Route::get('/url/{id}/home', [UrlController::class, 'get_home_links']);
-            Route::get('/url/{id}/subpage/{subId}', [UrlController::class, 'get_subpage']);
-            Route::get('/url/{id}/subpage/{subId}/home', [UrlController::class, 'get_subpage_links']);
-        }
-    );
+        /*****Url tespit İşlemleri */
+        Route::resource('url', UrlController::class);
+        Route::post('/url/ajax', [UrlController::class, 'get_ajax']);
+        Route::get('/url/{id}/home', [UrlController::class, 'get_home_links']);
+        Route::get('/url/{id}/subpage/{subId}', [UrlController::class, 'get_subpage']);
+        Route::get('/url/{id}/subpage/{subId}/home', [UrlController::class, 'get_subpage_links']);
+    });
 });
