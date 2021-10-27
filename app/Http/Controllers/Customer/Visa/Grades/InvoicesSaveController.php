efc12eea77696cbe8b192f2a8f96b3ff5c555b09
Rplus Media <rplusmediaweb@gmail.com>
2021-10-27 16:30:54 +0300

    Boşluklar silindi

--- app/Http/Controllers/Customer/Visa/Grades/InvoicesSaveController.php
+++ app/Http/Controllers/Customer/Visa/Grades/InvoicesSaveController.php
@@ -17,59 +17,35 @@ class InvoicesSaveController extends Controller
      */
     public function index($id, $visa_file_id)
     {
-        $baseCustomerDetails = DB::table('customers')
-            ->select(
-                [
-                    'customers.id AS id',
-                    'visa_files.id AS visa_file_id',
-                ]
-            )
+        $baseCustomerDetails = DB::table('customers')->select([
+            'customers.id AS id',
+            'visa_files.id AS visa_file_id',
+        ])
             ->join('visa_files', 'visa_files.customer_id', '=', 'customers.id')
             ->where('visa_files.active', '=', 1)
             ->where('customers.id', '=', $id)->first();
 
-
-        $invoices = DB::table('visa_invoices')
-            ->select(
-                "visa_invoices.id AS id",
-                "visa_invoices.payment AS payment",
-                "visa_invoices.matrah AS matrah",
-                "visa_invoices.date AS date",
-                "visa_invoices.number AS number",
-                "visa_invoices.created_at AS created_at",
-                "users.name"
-            )
+        $invoices = DB::table('visa_invoices')->select([
+            "visa_invoices.id AS id",
+            "visa_invoices.payment AS payment",
+            "visa_invoices.matrah AS matrah",
+            "visa_invoices.date AS date",
+            "visa_invoices.number AS number",
+            "visa_invoices.created_at AS created_at",
+            "users.name"
+        ])
             ->Join("visa_files", "visa_files.id", "=", "visa_invoices.visa_file_id")
             ->leftJoin("users", "users.id", "=", "visa_invoices.user_id")
             ->where("visa_invoices.visa_file_id", "=", $visa_file_id)
             ->distinct()
             ->get();;
 
-
-        return view('customer.visa.grades.invoices-save')->with(
-            [
-                'baseCustomerDetails' => $baseCustomerDetails,
-                'invoices' => $invoices,
-            ]
-        );
+        return view('customer.visa.grades.invoices-save')->with([
+            'baseCustomerDetails' => $baseCustomerDetails,
+            'invoices' => $invoices,
+        ]);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request, $id, $visa_file_id)
     {
         $matrah_kurus = "00";
@@ -91,18 +67,16 @@ class InvoicesSaveController extends Controller
             $odeme = $request->input('odeme') . "." . $odeme_kurus;
         }
 
-        if (
-            DB::table('visa_invoices')->insert([
-                'visa_file_id' => $visa_file_id,
-                'user_id' => $request->session()->get('userId'),
-                'visa_file_id' => $visa_file_id,
-                'payment' => $odeme,
-                'matrah' => $matrah,
-                'date' => $request->input('tarih'),
-                'number' => $request->input('numara'),
-                'created_at' => date('Y-m-d H:i:s'),
-            ])
-        ) {
+        if (DB::table('visa_invoices')->insert([
+            'visa_file_id' => $visa_file_id,
+            'user_id' => $request->session()->get('userId'),
+            'visa_file_id' => $visa_file_id,
+            'payment' => $odeme,
+            'matrah' => $matrah,
+            'date' => $request->input('tarih'),
+            'number' => $request->input('numara'),
+            'created_at' => date('Y-m-d H:i:s'),
+        ])) {
             $request->session()
                 ->flash('mesajSuccess', 'Başarıyla eklendi');
 
@@ -114,54 +88,10 @@ class InvoicesSaveController extends Controller
         }
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($id, $visa_file_id, $invoice_id, Request $request)
     {
         if (is_numeric($invoice_id)) {
-            if (
-                DB::table('visa_invoices')
-                ->where('id', '=', $invoice_id)
-                ->delete()
-            ) {
+            if (DB::table('visa_invoices')->where('id', '=', $invoice_id)->delete()) {
                 $request->session()
                     ->flash('mesajSuccess', 'Başarıyla silindi');
 
@@ -177,6 +107,7 @@ class InvoicesSaveController extends Controller
             return redirect('/musteri/' . $id . '/vize/' . $visa_file_id . '/fatura-kayit');
         }
     }
+
     public function tamamla($id, $visa_file_id, Request $request)
     {
         $visaFileGradesId = DB::table('visa_files')
@@ -200,20 +131,14 @@ class InvoicesSaveController extends Controller
                 'created_at' => date('Y-m-d H:i:s'),
             ]);
 
-            if (DB::table('visa_files')
-                ->where("id", "=", $visa_file_id)
-                ->update([
-                    'visa_file_grades_id' => $nextGrades
-                ])
+            if (DB::table('visa_files')->where("id", "=", $visa_file_id)
+                ->update(['visa_file_grades_id' => $nextGrades])
             ) {
-
                 if ($request->session()->has($visa_file_id . '_grades_id')) {
                     $request->session()->forget($visa_file_id . '_grades_id');
                 }
-
                 $request->session()
                     ->flash('mesajSuccess', 'Kayıt başarıyla yapıldı');
-
                 return redirect('/musteri/' . $id . '/vize/');
             } else {
                 $request->session()
