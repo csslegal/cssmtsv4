d3111243bcd9fe198ab092655cc275d702e15156
Rplus Media <rplusmediaweb@gmail.com>
2021-11-11 16:21:56 +0300

    Button düzenlemesi yapıldı

--- resources/views/customer/visa/cards/visa-proccess.blade.php
+++ resources/views/customer/visa/cards/visa-proccess.blade.php
@@ -4,11 +4,11 @@
         <div class="row">
             @if (!isset($visaFileDetail) && $visaFileGradesPermitted)
                 <div class="col-lg-4 col-md-6 col-sm-6 ">
-                    <div class="card border-success mb-2">
+                    <div class="card border-primary mb-2">
                         <div class="card-body">
                             <h5 class="card-title">Cari Dosya Aç</h5>
                             <p>&nbsp;</p>
-                            <a class="confirm btn btn-success btn-sm float-end text-white" data-title="Dikkat!"
+                            <a class="confirm btn btn-primary btn-sm float-end" data-title="Dikkat!"
                                 data-content="Devam edilsin mi?" href="vize/dosya-ac"> İşleme Git</a>
                         </div>
                     </div>
@@ -16,33 +16,33 @@
             @endif
             @if (isset($visaFileDetail))
                 <div class="col-lg-4 col-md-6 col-sm-6 ">
-                    <div class="card border-primary mb-2">
+                    <div class="card border-secondary mb-2">
                         <div class="card-body">
                             <h5 class="card-title">Dosya Ödemeleri</h5>
                             <p>Cari dosya ödeme detayları</p>
-                            <a class="confirm btn btn-primary btn-sm float-end text-white" data-title="Dikkat!"
+                            <a class="confirm btn btn-secondary btn-sm float-end" data-title="Dikkat!"
                                 data-content="Devam edilsin mi?" href="vize/{{ $visaFileDetail->id }}/odeme"> İşleme
                                 Git</a>
                         </div>
                     </div>
                 </div>
-                <div class="col-lg-4 col-md-6 col-sm-6 ">
+                <!--<div class="col-lg-4 col-md-6 col-sm-6 ">
                     <div class="card border-success mb-2">
                         <div class="card-body">
                             <h5 class="card-title">Dosya Makbuzları</h5>
                             <p>Cari dosya makbuz detayları</p>
-                            <a class="confirm btn btn-success btn-sm float-end text-white" data-title="Dikkat!"
+                            <a class="confirm btn btn-success btn-sm float-end" data-title="Dikkat!"
                                 data-content="Devam edilsin mi?" href="vize/{{ $visaFileDetail->id }}/makbuz"> İşleme
                                 Git</a>
                         </div>
                     </div>
-                </div>
+                </div>-->
                 <div class="col-lg-4 col-md-6 col-sm-6 ">
                     <div class="card border-danger mb-2">
                         <div class="card-body">
                             <h5 class="card-title">Faturalar</h5>
                             <p>Cari dosya fatura detayları;</p>
-                            <a class="confirm btn btn-danger btn-sm float-end text-white" data-title="Dikkat!"
+                            <a class="confirm btn btn-danger btn-sm text-white float-end" data-title="Dikkat!"
                                 data-content="Devam edilsin mi?" href="vize/{{ $visaFileDetail->id }}/faturalar">
                                 İşleme Git</a>
                         </div>
@@ -51,22 +51,24 @@
             @endif
             @if (isset($visaFileDetail))
                 <div class="col-lg-4 col-md-6 col-sm-6 ">
-                    <div class="card border-danger mb-2">
+                    <div class="card border-warning mb-2">
                         <div class="card-body">
                             <h5 class="card-title">Dosya Kapatma</h5>
                             <p>Cari dosya kapatma</p>
-                            <a class="confirm btn btn-danger btn-sm float-end text-white" data-title="Dikkat!"
-                                data-content="Devam edilsin mi?" href="vize/{{ $visaFileDetail->id }}/kapat"> İşleme
-                                Git</a>
+                            <a class="confirm btn btn-warning btn-sm float-end text-dark" data-title="Dikkat!"
+                                data-content="Dosya kapatma işlemi! Devam edilsin mi?"
+                                href="vize/{{ $visaFileDetail->id }}/kapatma">
+                                İşleme Git
+                            </a>
                         </div>
                     </div>
                 </div>
                 <div class="col-lg-4 col-md-6 col-sm-6 ">
-                    <div class="card border-success mb-2">
+                    <div class="card border-info mb-2">
                         <div class="card-body">
-                            <h5 class="card-title">Arşive Taşıma</h5>
+                            <h5 class="card-title">Direk Arşive Taşıma</h5>
                             <p>Cari dosyayı arşive taşıma</p>
-                            <a class="confirm btn btn-success btn-sm float-end text-white" data-title="Dikkat!"
+                            <a class="confirm btn btn-info btn-sm float-end" data-title="Dikkat!"
                                 data-content="Dosya direk arşive taşınacak! Devam edilsin mi?"
                                 href="vize/{{ $visaFileDetail->id }}/arsive-tasima">
                                 İşleme Git</a>
@@ -76,11 +78,11 @@
             @endif
 
             <div class="col-lg-4 col-md-6 col-sm-6 ">
-                <div class="card border-primary mb-2">
+                <div class="card border-dark mb-2">
                     <div class="card-body">
                         <h5 class="card-title">Arşiv Dosyaları</h5>
                         <p>Pasif dosya detayları</p>
-                        <a class="confirm btn btn-primary btn-sm float-end text-white" data-title="Dikkat!"
+                        <a class="confirm btn btn-dark btn-sm float-end" data-title="Dikkat!"
                             data-content="Devam edilsin mi?" href="vize/arsiv"> İşleme Git</a>
                     </div>
                 </div>
