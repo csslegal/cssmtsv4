b09df4354dde90bf7fbe46e2dfffdd398eff5fa7
Rplus Media <rplusmediaweb@gmail.com>
2021-11-11 09:09:43 +0300

    input aralıkları düzenlendi

--- resources/views/management/visa/file-grades/create.blade.php
+++ resources/views/management/visa/file-grades/create.blade.php
@@ -14,44 +14,45 @@
         <div class="card-body">
             <form method="POST" action="/yonetim/vize/dosya-asama">
                 @csrf
-
-                <div class="col-12">
-                    <label class="form-label"> Adı</label>
-                    <input type="text" value="{{ old('name') }}" name="name" class="form-control">
-                    @error('name')
-                        <div class="alert alert-danger">{{ $message }}</div>
-                    @enderror
-                </div>
-                <div class="col-12">
-                    <label class="form-label">Url</label>
-                    <input type="text" value="{{ old('url') }}" name="url" class="form-control">
-                    @error('url')
-                        <div class="alert alert-danger">{{ $message }}</div>
-                    @enderror
-                </div>
-                <div class="col-12">
-                    <label class="form-label">Env Bağlantısı</label>
-                    <select name="env" class="form-control">
-                        <option value="">Seçim yapınız</option>
-                        <option value="VISA_FILE_OPEN_GRADES_ID">Dosya Açma İşlemi</option>
-                        <option value="VISA_PAYMENT_CONFIRM_GRADES_ID">Alınan Ödeme Onayı İşlemi</option>
-                        <option value="VISA_TRANSLATOR_AUTH_GRADES_ID">Tercüme Yetkilendirme</option>
-                        <option value="VISA_TRANSLATION_GRADES_ID">Tercüme Tamamlama İşlemi</option>
-                        <option value="VISA_EXPERT_AUTH_GRADES_ID">Uzman Yetkilendirme İşlemi</option>
-                        <option value="VISA_APPOINTMENT_GRADES_ID">Randevu Bilgileri İşlemi</option>
-                        <option value="VISA_DACTYLOGRAM_GRADES_ID">Parmak İzi İşlemi</option>
-                        <option value="VISA_MADE_PAYMENT_GRADES_ID">Yapılan Ödeme İşlemi</option>
-                        <option value="VISA_INVOICE_SAVE_GRADES_ID">Fatura Kayıt İşlemi</option>
-                        <option value="VISA_APPOINTMENT_PUTOFF_GRADES_ID">Randevu Erteleme İşlemi</option>
-                        <option value="VISA_APPOINTMENT_CANCEL_GRADES_ID">Randevu İptali İşlemi</option>
-                        <option value="VISA_RE_PAYMENT_CONFIRM_GRADES_ID">Yeniden Alınan Ödeme Onayı İşlemi</option>
-                        <option value="VISA_FILE_DELIVERY_GRADES_ID">Dosya Teslim İşlemi</option>
-                        <option value="VISA_FILE_CLOSED_GRADES_ID">Dosya Kapama İşlemi</option>
-                        <option value="VISA_FILE_REFUSAL_GRADES_ID">Vize Reddi Tercüme İşlemi</option>
-                    </select>
-                    @error('env')
-                        <div class="alert alert-danger">{{ $message }}</div>
-                    @enderror
+                <div class="row g-3">
+                    <div class="col-12">
+                        <label class="form-label"> Adı</label>
+                        <input type="text" value="{{ old('name') }}" name="name" class="form-control">
+                        @error('name')
+                            <div class="alert alert-danger">{{ $message }}</div>
+                        @enderror
+                    </div>
+                    <div class="col-12">
+                        <label class="form-label">Url</label>
+                        <input type="text" value="{{ old('url') }}" name="url" class="form-control">
+                        @error('url')
+                            <div class="alert alert-danger">{{ $message }}</div>
+                        @enderror
+                    </div>
+                    <div class="col-12">
+                        <label class="form-label">Env Bağlantısı</label>
+                        <select name="env" class="form-control">
+                            <option value="">Seçim yapınız</option>
+                            <option value="VISA_FILE_OPEN_GRADES_ID">Dosya Açma İşlemi</option>
+                            <option value="VISA_PAYMENT_CONFIRM_GRADES_ID">Alınan Ödeme Onayı İşlemi</option>
+                            <option value="VISA_TRANSLATOR_AUTH_GRADES_ID">Tercüme Yetkilendirme</option>
+                            <option value="VISA_TRANSLATION_GRADES_ID">Tercüme Tamamlama İşlemi</option>
+                            <option value="VISA_EXPERT_AUTH_GRADES_ID">Uzman Yetkilendirme İşlemi</option>
+                            <option value="VISA_APPOINTMENT_GRADES_ID">Randevu Bilgileri İşlemi</option>
+                            <option value="VISA_DACTYLOGRAM_GRADES_ID">Parmak İzi İşlemi</option>
+                            <option value="VISA_MADE_PAYMENT_GRADES_ID">Yapılan Ödeme İşlemi</option>
+                            <option value="VISA_INVOICE_SAVE_GRADES_ID">Fatura Kayıt İşlemi</option>
+                            <option value="VISA_APPOINTMENT_PUTOFF_GRADES_ID">Randevu Erteleme İşlemi</option>
+                            <option value="VISA_APPOINTMENT_CANCEL_GRADES_ID">Randevu İptali İşlemi</option>
+                            <option value="VISA_RE_PAYMENT_CONFIRM_GRADES_ID">Yeniden Alınan Ödeme Onayı İşlemi</option>
+                            <option value="VISA_FILE_DELIVERY_GRADES_ID">Dosya Teslim İşlemi</option>
+                            <option value="VISA_FILE_CLOSED_GRADES_ID">Dosya Kapama İşlemi</option>
+                            <option value="VISA_FILE_REFUSAL_GRADES_ID">Vize Reddi Tercüme İşlemi</option>
+                        </select>
+                        @error('env')
+                            <div class="alert alert-danger">{{ $message }}</div>
+                        @enderror
+                    </div>
                 </div>
                 <button class="w-100 mt-3 btn btn-danger text-white btn-lg" type="submit">Tamamla</button>
             </form>
