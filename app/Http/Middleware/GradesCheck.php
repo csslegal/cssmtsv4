f98316acb089908c3cb528b820badfe516e4b4b0
Rplus Media <rplusmediaweb@gmail.com>
2021-11-25 12:54:44 +0300

    Cari aşamaya göre işlem yapılacak sayfa kontrolu

--- app/Http/Middleware/GradesCheck.php
+++ app/Http/Middleware/GradesCheck.php
@@ -10,18 +10,39 @@ class GradesCheck
 {
     public function handle(Request $request, Closure $next)
     {
-        /**Eğer dosya ref no ait dosya aşama var mı yo mu*/
+        $theLastedURL = request()->segment(count(request()->segments()));
 
-        if (
-            DB::table('visa_files')
-            ->where('customer_id', '=', $request->id)
-            ->where('id', '=', $request->visa_file_id)
-            ->where('active', '=', 1)
-            ->get()->count() == 0
-        ) {
+        $arrayURLS = array(
+            'fatura',
+            'odeme',
+            'kapatma',
+            'arsive-tasima',
+            'alinan-odeme-tamamla',
+            'yapilan-odeme-tamamla',
+            'fatura-kayit-tamamla',
+            'yeniden-alinan-odeme-tamamla',
+            'iade-bilgileri-tamamla',
+        );
 
-            $request->session()->flash('mesajSuccess','Hatalı istek yapıldı');
-            return redirect('/musteri/' . $request->id);
+        $visaDetail = DB::table('visa_files')
+            ->select([
+                'visa_files.visa_file_grades_id',
+                'visa_file_grades.url'
+            ])
+            ->join('visa_file_grades', 'visa_file_grades.id', '=', 'visa_files.visa_file_grades_id')
+            ->where('visa_files.customer_id', '=', $request->id)
+            ->where('visa_files.id', '=', $request->visa_file_id)
+            ->where('visa_files.active', '=', 1)
+            ->first();
+
+        /**Eğer ( dosya ref no ait dosya aşama yoksa ) veya ( istek yapılan url sistemde yok ) ıse hatalı istek olacak*/
+        if ($visaDetail == null || $visaDetail->url != $theLastedURL) {
+
+            /****istisna olarak hatalı fakat genel işlemlerden birisi değilse hatayı devam ettirecek */
+            if (!in_array($theLastedURL, $arrayURLS)) {
+                $request->session()->flash('mesajDanger', 'Hatalı istek yapıldı');
+                return redirect('/musteri/' . $request->id);
+            }
         }
         return $next($request);
     }
