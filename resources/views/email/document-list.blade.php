33e5880a8af410b13910aa7b0ed6cf3fb0ba0f2f
Rplus Media <rplusmediaweb@gmail.com>
2021-12-08 10:47:43 +0300

    Url güncellemesi yapıldı

--- resources/views/email/document-list.blade.php
+++ resources/views/email/document-list.blade.php
@@ -1,7 +1,7 @@
 <p>Değerli Müşterimiz,</p>
 <p>
     İlgili evrak listesini ekte bulabilirsiniz. Daha fazla bilgi için kurumsal web sitemizi
-    <a href="https:://csslegal.com?utm_source=email&utm_medium=docment-list">buradan</a>
+    <a href="https://csslegal.com?utm_source=email&utm_medium=docment-list">buradan</a>
     ziyaret edebilirsiniz.
 </p>
 <p>CSS & Co Legal Services</p>
