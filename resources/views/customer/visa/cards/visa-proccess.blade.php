15854b2c9613dc3db5feb4a9a240465c60006626
Rplus Media <rplusmediaweb@gmail.com>
2022-01-19 16:01:58 +0300

    Vize işlemleri yetkileri düzenlendi

--- resources/views/customer/visa/cards/visa-proccess.blade.php
+++ resources/views/customer/visa/cards/visa-proccess.blade.php
@@ -1,88 +1,80 @@
-<div class="card card-primary mb-3">
-    <div class="card-header bg-primary text-white fw-bold">Vize Dosya İşlemleri</div>
-    <div class="card-body scroll">
-        <div class="row">
-            @if (!isset($visaFileDetail) && $visaFileGradesPermitted['permitted'])
-                <div class="col-lg-4 col-md-6 col-sm-6 ">
-                    <div class="card border-primary mb-2">
-                        <div class="card-body">
-                            <h5 class="card-title fw-bold">Cari Dosya Aç</h5>
-                            <p>&nbsp;</p>
-                            <a class="btn btn-primary btn-sm float-end" href="vize/dosya-ac"> İşleme Git</a>
-                        </div>
-                    </div>
-                </div>
-            @endif
-            @if (isset($visaFileDetail))
-                <div class="col-lg-4 col-md-6 col-sm-6 ">
-                    <div class="card mb-2">
-                        <div class="card-body">
-                            <h5 class="card-title fw-bold">Ödemeler</h5>
-                            <p>Cari dosya ödeme detayları</p>
-                            <a class="btn btn-primary btn-sm float-end"
-                                href="vize/{{ $visaFileDetail->id }}/odeme">İşleme Git</a>
-                        </div>
-                    </div>
-                </div>
-                <!--<div class="col-lg-4 col-md-6 col-sm-6 ">
-                    <div class="card border-success mb-2">
-                        <div class="card-body">
-                            <h5 class="card-title fw-bold">Makbuzlar</h5>
-                            <p>Cari dosya makbuz detayları</p>
-                            <a class="btn btn-success btn-sm float-end" href="vize/{{ $visaFileDetail->id }}/makbuz"> İşleme Git</a>
-                        </div>
-                    </div>
-                </div>-->
-                <div class="col-lg-4 col-md-6 col-sm-6 ">
-                    <div class="card mb-2">
-                        <div class="card-body">
-                            <h5 class="card-title fw-bold">Faturalar</h5>
-                            <p>Cari dosya fatura detayları</p>
-                            <a class="btn btn-primary btn-sm text-white float-end"
-                                href="vize/{{ $visaFileDetail->id }}/fatura">İşleme Git</a>
-                        </div>
-                    </div>
-                </div>
-            @endif
-            @if (isset($visaFileDetail))
-
-                @if (!$visaFileGradesPermitted['fileCloseRequestGradeIds'] && $visaFileGradesPermitted['fileCloseRequestGrade'])
+@if (session('userTypeId') == 1 || session('userTypeId') == 2 || session('userTypeId') == 4 || session('userTypeId') == 6 || session('userTypeId') == 7)
+    <div class="card card-primary mb-3">
+        <div class="card-header bg-primary text-white fw-bold">Vize Dosya İşlemleri</div>
+        <div class="card-body scroll">
+            <div class="row">
+                @if (!isset($visaFileDetail) && $visaFileGradesPermitted['permitted'])
                     <div class="col-lg-4 col-md-6 col-sm-6 ">
-                        <div class="card mb-2">
+                        <div class="card border-primary mb-2">
                             <div class="card-body">
-                                <h5 class="card-title fw-bold">Dosya Kapatma</h5>
-                                <p>Cari dosya kapatma isteği</p>
-                                <a class="confirm btn btn-primary btn-sm float-end" data-title="Dikkat!"
-                                    data-content="Dosya kapatma isteği işlemi! Devam edilsin mi?"
-                                    href="vize/{{ $visaFileDetail->id }}/kapatma">İşleme Yap</a>
+                                <h5 class="card-title fw-bold">Cari Dosya Aç</h5>
+                                <p>&nbsp;</p>
+                                <a class="btn btn-primary btn-sm float-end" href="vize/dosya-ac"> İşleme Git</a>
                             </div>
                         </div>
                     </div>
                 @endif
+                @if (isset($visaFileDetail))
 
+                    <div class="col-lg-4 col-md-6 col-sm-6 ">
+                        <div class="card mb-2">
+                            <div class="card-body">
+                                <h5 class="card-title fw-bold">Ödemeler</h5>
+                                <p>Cari dosya ödeme detayları</p>
+                                <a class="btn btn-primary btn-sm float-end"
+                                    href="vize/{{ $visaFileDetail->id }}/odeme">İşleme Git</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-4 col-md-6 col-sm-6 ">
+                        <div class="card mb-2">
+                            <div class="card-body">
+                                <h5 class="card-title fw-bold">Faturalar</h5>
+                                <p>Cari dosya fatura detayları</p>
+                                <a class="btn btn-primary btn-sm text-white float-end"
+                                    href="vize/{{ $visaFileDetail->id }}/fatura">İşleme Git</a>
+                            </div>
+                        </div>
+                    </div>
+
+                    @if (!$visaFileGradesPermitted['fileCloseRequestGradeIds'] && $visaFileGradesPermitted['fileCloseRequestGrade'])
+                        <div class="col-lg-4 col-md-6 col-sm-6 ">
+                            <div class="card mb-2">
+                                <div class="card-body">
+                                    <h5 class="card-title fw-bold">Dosya Kapatma</h5>
+                                    <p>Cari dosya kapatma isteği</p>
+                                    <a class="confirm btn btn-primary btn-sm float-end" data-title="Dikkat!"
+                                        data-content="Dosya kapatma isteği işlemi! Devam edilsin mi?"
+                                        href="vize/{{ $visaFileDetail->id }}/kapatma">İşleme Yap</a>
+                                </div>
+                            </div>
+                        </div>
+                    @endif
+                    @if (session('userTypeId') == 1)
+                        <div class="col-lg-4 col-md-6 col-sm-6 ">
+                            <div class="card mb-2">
+                                <div class="card-body">
+                                    <h5 class="card-title fw-bold">Arşive Taşı</h5>
+                                    <p>Cari dosyayı arşive taşıma</p>
+                                    <a class="confirm btn btn-primary btn-sm float-end" data-title="Dikkat!"
+                                        data-content="Dosya direk arşive taşınacak! Devam edilsin mi?"
+                                        href="vize/{{ $visaFileDetail->id }}/arsive-tasima">İşleme Git</a>
+                                </div>
+                            </div>
+                        </div>
+                    @endif
+                @endif
                 <div class="col-lg-4 col-md-6 col-sm-6 ">
                     <div class="card mb-2">
                         <div class="card-body">
-                            <h5 class="card-title fw-bold">Arşive Taşı</h5>
-                            <p>Cari dosyayı arşive taşıma</p>
-                            <a class="confirm btn btn-primary btn-sm float-end" data-title="Dikkat!"
-                                data-content="Dosya direk arşive taşınacak! Devam edilsin mi?"
-                                href="vize/{{ $visaFileDetail->id }}/arsive-tasima">İşleme Git</a>
+                            <h5 class="card-title fw-bold">Arşivler</h5>
+                            <p>Pasif dosya detayları</p>
+                            <a class="btn btn-primary btn-sm float-end" href="vize/arsiv"> İşleme Git</a>
                         </div>
                     </div>
                 </div>
-            @endif
-
-            <div class="col-lg-4 col-md-6 col-sm-6 ">
-                <div class="card mb-2">
-                    <div class="card-body">
-                        <h5 class="card-title fw-bold">Arşivler</h5>
-                        <p>Pasif dosya detayları</p>
-                        <a class="btn btn-primary btn-sm float-end" href="vize/arsiv"> İşleme Git</a>
-                    </div>
-                </div>
             </div>
-
         </div>
     </div>
-</div>
+@endif
