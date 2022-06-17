ab104f222c4c03ce706cb69947f612e36e9d870c
Rplus Media <rplusmediaweb@gmail.com>
2022-06-17 12:52:36 +0300

    Evrak listesi gönderilme işlemi güncellendi.

--- app/Http/Controllers/Customer/Visa/Grades/DocumentListEmailSendController.php
+++ app/Http/Controllers/Customer/Visa/Grades/DocumentListEmailSendController.php
@@ -12,12 +12,43 @@ use Illuminate\Support\Facades\Mail;
 
 class DocumentListEmailSendController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index($id, $visa_file_id)
+    public function index($id, $visa_file_id, Request $request)
+    {
+        $visaFileGradesId = DB::table('visa_files')->select(['visa_file_grades_id'])->where('id', '=', $visa_file_id)->first();
+        $visaFileGradesName = new VisaFileGradesName($visaFileGradesId->visa_file_grades_id);
+
+        $whichGrades = new VisaFileWhichGrades();
+        $nextGrades = $whichGrades->nextGrades($visa_file_id);
+
+        if ($nextGrades != null) {
+
+            if (DB::table('visa_files')->where("id", "=", $visa_file_id)->update(['visa_file_grades_id' => $nextGrades])) {
+
+                if ($request->session()->has($visa_file_id . '_grades_id')) {
+                    $request->session()->forget($visa_file_id . '_grades_id');
+                }
+
+                DB::table('visa_file_logs')->insert([
+                    'visa_file_id' => $visa_file_id,
+                    'user_id' => $request->session()->get('userId'),
+                    'subject' => $visaFileGradesName->getName(),
+                    'content' => 'Evrak listesi gönderildi',
+                    'created_at' => date('Y-m-d H:i:s'),
+                ]);
+
+                $request->session()->flash('mesajSuccess', 'Evrak listesi gönderildi');
+                return redirect('/musteri/' . $id . '/vize');
+            } else {
+                $request->session()->flash('mesajInfo', 'Güncelleme sırasında sorun oluştu');
+                return redirect('/musteri/' . $id . '/vize');
+            }
+        } else {
+            $request->session()->flash('mesajDanger', 'Sonraki vize aşama bulunamadı');
+            return redirect('/musteri/' . $id . '/vize');
+        }
+    }
+
+    public function index_islem($id, $visa_file_id)
     {
         $baseCustomerDetails = DB::table('customers')
             ->select([
