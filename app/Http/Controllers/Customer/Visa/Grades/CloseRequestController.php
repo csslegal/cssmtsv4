82dc758f1d62fe71a965a41795f583cd4d4f8896
Rplus Media <rplusmediaweb@gmail.com>
2022-06-29 09:50:03 +0300

    Dosya kapama aşama güncellendi

--- app/Http/Controllers/Customer/Visa/Grades/CloseRequestController.php
+++ app/Http/Controllers/Customer/Visa/Grades/CloseRequestController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Customer\Visa\Grades;
 
 use App\Http\Controllers\Controller;
 use App\MyClass\VisaFileGradesName;
+use App\MyClass\VisaFileWhichGrades;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -16,7 +17,6 @@ class CloseRequestController extends Controller
      */
     public function index($id, $visa_file_id, Request $request)
     {
-
         $visaFiles = DB::table('visa_files')->where('id', '=', $visa_file_id)->first();
         $visaApplicationResultCount = DB::table('visa_application_result')->where('visa_file_id', '=', $visa_file_id)->get()->count();
 
@@ -48,17 +48,29 @@ class CloseRequestController extends Controller
             'created_at' => date('Y-m-d H:i:s'),
         ]);
 
-        DB::table('visa_files')->where("id", "=", $visa_file_id)
-            ->update([
-                'visa_file_grades_id' => env('VISA_FILE_CLOSE_CONFIRM_GRADES_ID'),
-                'temp_grades_id' => $visaFiles->visa_file_grades_id,
+        DB::table('visa_files')->where("id", "=", $visa_file_id)->update([
+            'visa_file_grades_id' => env('VISA_FILE_CLOSE_REQUEST_GRADES_ID'),
+            'temp_grades_id' => $visaFiles->visa_file_grades_id,
+        ]);
+
+        $whichGrades = new VisaFileWhichGrades();
+        $nextGrades = $whichGrades->nextGrades($visa_file_id);
+
+        if ($nextGrades != null) {
+
+            DB::table('visa_files')->where("id", "=", $visa_file_id)->update([
+                'visa_file_grades_id' => $nextGrades,
             ]);
 
-        if ($request->session()->has($visa_file_id . '_grades_id')) {
-            $request->session()->forget($visa_file_id . '_grades_id');
-        }
+            if ($request->session()->has($visa_file_id . '_grades_id')) {
+                $request->session()->forget($visa_file_id . '_grades_id');
+            }
 
-        $request->session()->flash('mesajSuccess', 'Kayıt başarıyla yapıldı');
-        return redirect('/musteri/' . $id . '/vize');
+            $request->session()->flash('mesajSuccess', 'Kayıt başarıyla yapıldı');
+            return redirect('/musteri/' . $id . '/vize');
+        } else {
+            $request->session()->flash('mesajDanger', 'Sonraki aşama bulunamadı');
+            return redirect('/musteri/' . $id . '/vize');
+        }
     }
 }
