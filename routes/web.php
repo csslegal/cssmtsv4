1f755aca3b769c24b90cba0177cad7438bae0f9a
Rplus Media <rplusmediaweb@gmail.com>
2022-08-04 15:42:45 +0300

    Sadeleştirme için gerekli silme işlemleri

--- routes/web.php
+++ routes/web.php
@@ -4,9 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\General\LoginController as GeneralLoginController;
 use App\Http\Controllers\Management\IndexController as ManagementIndexController;
-use App\Http\Controllers\Management\NoticeController as ManagementNoticeController;
 use App\Http\Controllers\Management\ProfilController as ManagementProfilController;
-use App\Http\Controllers\Management\LanguageController as ManagementLanguageController;
 use App\Http\Controllers\Management\CustomersController as ManagementCustomersController;
 use App\Http\Controllers\Management\AjaxController as ManagementAjaxController;
 use App\Http\Controllers\Management\ApplicationOfficeController as ManagementApplicationOfficeController;
@@ -21,10 +19,7 @@ use App\Http\Controllers\Management\WebPanelsController as ManagementWebPanelsCo
 use App\Http\Controllers\Management\WebPanelAuthController as ManagementWebPanelAuthController;
 
 use App\Http\Controllers\Management\VisaController as ManagementVisaController;
-use App\Http\Controllers\Management\VisaEmailsDocumentListController as ManagementVisaEmailsDocumentListController;
-use App\Http\Controllers\Management\VisaSubTypesController as ManagementVisaSubTypesController;
 use App\Http\Controllers\Management\VisaTypesController as ManagementVisaTypesController;
-use App\Http\Controllers\Management\VisaEmailsInformationController as ManagementVisaEmailsInformationController;
 use App\Http\Controllers\Management\VisaFileGradesController as ManagementVisaFileGradesController;
 use App\Http\Controllers\Management\VisaFileGradesUsersTypeController as ManagementVisaFileGradesUsersTypeController;
 use App\Http\Controllers\Management\VisaValidityController as ManagementVisaValidityController;
@@ -38,42 +33,20 @@ use App\Http\Controllers\User\AjaxController as UserAjaxController;
 use App\Http\Controllers\Customer\IndexController as CustomerIndexController;
 use App\Http\Controllers\Customer\SearchController as CustomerSearchController;
 use App\Http\Controllers\Customer\NoteController as CustomerNoteController;
-use App\Http\Controllers\Customer\EditRequestController as CustomerEditRequestController;
 use App\Http\Controllers\Customer\LogsController as CustomerLogsController;
 use App\Http\Controllers\Customer\AjaxController as CustomerAjaxController;
 use App\Http\Controllers\Customer\Visa\IndexController as VisaIndexController;
 use App\Http\Controllers\Customer\Visa\GradesUpdateController as VisaGradesUpdateController;
-use App\Http\Controllers\Customer\Visa\InformationEmailController as VisaInformationEmailController;
 use App\Http\Controllers\Customer\Visa\Grades\FileOpenController as VisaFileOpenController;
-use App\Http\Controllers\Customer\Visa\Grades\ReceivedPaymentsController as VisaReceivedPaymentsController;
-use App\Http\Controllers\Customer\Visa\Grades\ReceivedPaymentsConfirmController as VisaReceivedPaymentsConfirmController;
-use App\Http\Controllers\Customer\Visa\Grades\DocumentListEmailSendController as VisaDocumentListEmailSendController;
 use App\Http\Controllers\Customer\Visa\Grades\DocumentListCompletedController as VisaDocumentListCompletedController;
-use App\Http\Controllers\Customer\Visa\Grades\TranslatorAuthController as VisaTranslatorAuthController;
 use App\Http\Controllers\Customer\Visa\Grades\TranslationsCompletedController as VisaTranslationsCompletedController;
-use App\Http\Controllers\Customer\Visa\Grades\ExpertAuthController as VisaExpertAuthController;
 use App\Http\Controllers\Customer\Visa\Grades\AppointmentCompletedController as VisaAppointmentCompletedController;
-use App\Http\Controllers\Customer\Visa\Grades\MadePaymentsController as VisaMadePaymentsController;
 use App\Http\Controllers\Customer\Visa\Grades\DactylogramController as VisaDactylogramController;
 
-use App\Http\Controllers\Customer\Visa\Grades\AppointmentCancelController as VisaAppointmentCancelController;
-
 use App\Http\Controllers\Customer\Visa\Grades\AppointmentPutOffController as VisaAppointmentPutOffController;
-use App\Http\Controllers\Customer\Visa\Grades\ReReceivedPaymentsController as VisaReReceivedPaymentsController;
-use App\Http\Controllers\Customer\Visa\Grades\ReReceivedPaymentsConfirmController as VisaReReceivedPaymentsConfirmController;
-
-use App\Http\Controllers\Customer\Visa\Grades\InvoicesSaveController as VisaInvoicesSaveController;
 use App\Http\Controllers\Customer\Visa\Grades\ApplicationResultController as VisaApplicationResultController;
 use App\Http\Controllers\Customer\Visa\Grades\FileDeliveryController as VisaFileDeliveryController;
-use App\Http\Controllers\Customer\Visa\Grades\RefusalTranslationController as VisaRefusalTranslationController;
 
-use App\Http\Controllers\Customer\Visa\Grades\CloseRequestController as VisaCloseRequestController;
-use App\Http\Controllers\Customer\Visa\Grades\CloseConfirmController as VisaCloseConfirmController;
-use App\Http\Controllers\Customer\Visa\Grades\RefundPaymentsController as VisaRefundPaymentsController;
-use App\Http\Controllers\Customer\Visa\Grades\RefundPaymentsConfirmController as VisaRefundPaymentsConfirmController;
-
-use App\Http\Controllers\Customer\Visa\PaymentsController as VisaPaymentsController;
-use App\Http\Controllers\Customer\Visa\InvoicesController as VisaInvoicesController;
 use App\Http\Controllers\Customer\Visa\ArchivesController as VisaArchivesController;
 use App\Http\Controllers\Customer\Visa\ArchiveTransportController as VisaArchiveTransportController;
 
@@ -97,60 +70,33 @@ Route::middleware(['sessionCheck'])->group(function () {
     Route::group(['prefix' => 'musteri'], function () {
 
         Route::resource('{id}/not-ekle', CustomerNoteController::class);
-        Route::resource('{id}/duzenle-istek', CustomerEditRequestController::class);
         Route::resource('{id}/logs', CustomerLogsController::class);
 
         /**Vize işlemleri*/
         Route::group(['prefix' => '{id}/vize'], function () {
             Route::get('/', [VisaIndexController::class, 'index']);
             Route::get('asama', [CustomerAjaxController::class, 'get_grades']);
-            Route::resource('bilgi-emaili', VisaInformationEmailController::class);
             Route::resource('arsiv', VisaArchivesController::class);
 
             /**Dosya aşama işlemleri */
-            Route::resource('dosya-ac', VisaFileOpenController::class);
+            Route::resource('dosya-acma', VisaFileOpenController::class);
 
             Route::group(['prefix' => '{visa_file_id}', 'middleware' => 'gradesCheck'], function () {
 
                 /***Dosya aşamalarından bağımsız bölümler */
+
                 Route::resource('asama-guncelle', VisaGradesUpdateController::class);
-                Route::resource('odeme', VisaPaymentsController::class);
-                Route::resource('fatura', VisaInvoicesController::class);
                 Route::resource('arsive-tasima', VisaArchiveTransportController::class);
 
                 /***Dosya aşamaları başlangıç */
-                Route::get('alinan-odeme-tamamla', [VisaReceivedPaymentsController::class, 'tamamla']);
-                Route::get('yapilan-odeme-tamamla', [VisaMadePaymentsController::class, 'tamamla']);
-                Route::get('fatura-kayit-tamamla', [VisaInvoicesSaveController::class, 'tamamla']);
-                Route::get('yeniden-alinan-odeme-tamamla', [VisaReReceivedPaymentsController::class, 'tamamla']);
-                Route::get('iade-bilgileri-tamamla', [VisaRefundPaymentsController::class, 'tamamla']);
-
-                Route::resource('alinan-odeme', VisaReceivedPaymentsController::class);
-                Route::resource('alinan-odeme-onay', VisaReceivedPaymentsConfirmController::class);
-                Route::resource('evrak-listesi', VisaDocumentListEmailSendController::class);
+                Route::resource('dosya-acma-onayi', VisaFileOpenConfirmController::class);
                 Route::resource('evrak-hazirlama', VisaDocumentListCompletedController::class);
-                Route::resource('tercuman-yetkilendir', VisaTranslatorAuthController::class);
                 Route::resource('tercume-tamamlama', VisaTranslationsCompletedController::class);
-                Route::resource('uzman-yetkilendir', VisaExpertAuthController::class);
                 Route::resource('form-bilgileri', VisaAppointmentCompletedController::class);
-                Route::resource('yapilan-odeme', VisaMadePaymentsController::class);
                 Route::resource('parmak-izi', VisaDactylogramController::class);
-
-                Route::resource('randevu-iptali', VisaAppointmentCancelController::class);
-
                 Route::resource('randevu-erteleme', VisaAppointmentPutoffController::class);
-                Route::resource('yeniden-alinan-odeme', VisaReReceivedPaymentsController::class);
-                Route::resource('yeniden-alinan-odeme-onay', VisaReReceivedPaymentsConfirmController::class);
-
-                Route::resource('fatura-kayit', VisaInvoicesSaveController::class);
                 Route::resource('basvuru-sonuc', VisaApplicationResultController::class);
-                Route::resource('red-tercume', VisaRefusalTranslationController::class);
                 Route::resource('teslimat-bilgisi', VisaFileDeliveryController::class);
-
-                Route::resource('kapatma', VisaCloseRequestController::class);
-                Route::resource('kapatma-onayi', VisaCloseConfirmController::class);
-                Route::resource('iade-bilgileri', VisaRefundPaymentsController::class);
-                Route::resource('iade-bilgileri-onayi', VisaRefundPaymentsConfirmController::class); //sonrasında teslimat aşamasına gececek
                 /***Dosya aşamaları son */
             });
         });
@@ -158,21 +104,15 @@ Route::middleware(['sessionCheck'])->group(function () {
         /**Musteri ajax işlemleri*/
         Route::group(['prefix' => 'ajax'], function () {
             Route::post('name-kontrol', [CustomerAjaxController::class, 'post_name_kontrol']);
-            Route::post('telefon-kontrol', [CustomerAjaxController::class, 'post_telefon_kontrol']);
+            Route::post('phone-kontrol', [CustomerAjaxController::class, 'post_telefon_kontrol']);
             Route::post('email-kontrol', [CustomerAjaxController::class, 'post_email_kontrol']);
             Route::post('not-goster', [CustomerAjaxController::class, 'post_not_goster']);
-            Route::post('email-log-goster', [CustomerAjaxController::class, 'post_email_log_goster']);
             Route::post('visa-log-goster', [CustomerAjaxController::class, 'post_visa_log_goster']);
             Route::post('not-sil', [CustomerAjaxController::class, 'post_not_sil']);
-            Route::post('alt-vize-tipi', [CustomerAjaxController::class, 'post_visa_sub_type']);
             Route::post('vize-dosya-log', [CustomerAjaxController::class, 'post_visa_file_log_content']);
 
             Route::group(['prefix' => 'vize'], function () {
-
                 Route::post('arsiv-log', [CustomerAjaxController::class, 'post_visa_archive_log']);
-                Route::post('arsiv-odeme', [CustomerAjaxController::class, 'post_visa_archive_payment']);
-                Route::post('arsiv-makbuz', [CustomerAjaxController::class, 'post_visa_archive_receipt']);
-                Route::post('arsiv-fatura', [CustomerAjaxController::class, 'post_visa_archive_invoice']);
             });
         });
     });
@@ -182,7 +122,6 @@ Route::middleware(['sessionCheck'])->group(function () {
         Route::get('/', [UserIndexController::class, 'get_index']);
         Route::get('profil', [UserIndexController::class, 'get_profil']);
         Route::post('profil', [UserIndexController::class, 'post_profil']);
-        Route::get('duyuru', [UserIndexController::class, 'get_duyuru']);
 
         /**web yönlendirmeleri */
         Route::prefix('web')->group(function () {
@@ -191,8 +130,7 @@ Route::middleware(['sessionCheck'])->group(function () {
 
         /*** Kullanıcılar ajax işlemleri*/
         Route::group(['prefix' => 'ajax'], function () {
-            Route::post('duyuru', [UserAjaxController::class, 'post_duyuru_icerik_cek']);
-            Route::get('duyuru-sayisi', [UserAjaxController::class, 'get_active_notice_count']);
+
         });
     });
 
@@ -201,7 +139,6 @@ Route::middleware(['sessionCheck'])->group(function () {
 
         Route::get('/', [ManagementIndexController::class, 'get_index']);
 
-        Route::resource('duyuru', ManagementNoticeController::class);
         Route::resource('profil', ManagementProfilController::class);
         Route::resource('users', ManagementUsersController::class);
         Route::resource('users-type', ManagementUsersTypeController::class);
@@ -209,13 +146,9 @@ Route::middleware(['sessionCheck'])->group(function () {
         Route::resource('application-office', ManagementApplicationOfficeController::class);
         Route::resource('appointment-office', ManagementAppointmentOfficeController::class);
         Route::resource('customers', ManagementCustomersController::class);
-        Route::resource('language', ManagementLanguageController::class);
 
         /**Yonetim ajax işlemleri*/
         Route::group(['prefix' => 'ajax'], function () {
-            Route::post('duyuru', [ManagementAjaxController::class, 'post_duyuru_cek']);
-            Route::post('bilgi-emaili', [ManagementAjaxController::class, 'post_bilgi_emaili_cek']);
-            Route::post('evrak-emaili', [ManagementAjaxController::class, 'post_evrak_emaili_cek']);
             Route::post('sirala', [ManagementAjaxController::class, 'post_sorting']);
             Route::post('dosya-asama-erisim', [ManagementAjaxController::class, 'post_visa_file_grades_users_type']);
             Route::post('panel-list', [ManagementAjaxController::class, 'post_panel_list']);
@@ -235,9 +168,6 @@ Route::middleware(['sessionCheck'])->group(function () {
 
             Route::resource('vize-tipi', ManagementVisaTypesController::class);
             Route::resource('vize-suresi', ManagementVisaValidityController::class);
-            Route::resource('alt-vize-tipi', ManagementVisaSubTypesController::class);
-            Route::resource('bilgi-emaili', ManagementVisaEmailsInformationController::class);
-            Route::resource('evrak-emaili', ManagementVisaEmailsDocumentListController::class);
             Route::resource('dosya-asama', ManagementVisaFileGradesController::class);
             Route::resource('dosya-asama-erisim', ManagementVisaFileGradesUsersTypeController::class);
         });
