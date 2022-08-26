53042e40534b1ad5fdd88dfc1f5a3182500f3d62
CSS Legal <rplusmediaweb@gmail.com>
2022-08-26 12:12:49 +0300

    Eski dosyalar kaldırıldı

--- webpack.mix.js
+++ webpack.mix.js
@@ -15,7 +15,6 @@ const mix = require('laravel-mix');
 mix.js('resources/js/app.js', 'public/js')
     .sass('resources/sass/app.scss', 'public/css').sourceMaps();
 
-mix.copy('node_modules/tinymce/skins', 'public/js/skins');
 mix.copy('node_modules/chart.js/dist', 'public/js/chart.js');
 mix.copy('node_modules/tinymce', 'public/js/tinymce');
 mix.copy('node_modules/air-datepicker', 'public/js/air-datepicker');
