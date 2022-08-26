3c150f5b705a27fb017037dfaf9ba62c60d388da
CSS Legal <rplusmediaweb@gmail.com>
2022-08-26 09:27:07 +0300

    Chart js düzenlemesi yapıldı

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,7 +13,7 @@ const mix = require('laravel-mix');
 
 
 mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css')
-    .sourceMaps();
+    .sass('resources/sass/app.scss', 'public/css').sourceMaps();
 
 mix.copy('node_modules/tinymce/skins', 'public/js/skins');
+mix.copy('node_modules/chart.js/dist', 'public/js/chart.js');
