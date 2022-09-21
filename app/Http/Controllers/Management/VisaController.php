bc212b1984a565e383656e6b65ef2a6006c9ce0b
CSS Legal <rplusmediaweb@gmail.com>
2022-09-21 15:47:27 +0300

    Tercüman, uzman ve danışman raporu tamamlandı

--- app/Http/Controllers/Management/VisaController.php
+++ app/Http/Controllers/Management/VisaController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers\Management;
 
 use App\Http\Controllers\Controller;
-use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -15,6 +14,17 @@ class VisaController extends Controller
             $explodes =  explode('--', $request->input('dates'));
         } else {
             $explodes = [date('Y-m-01'), date('Y-m-28')];
+        }
+
+        if ($request->has('status')) {
+
+            if ($request->input('status') == "cari") {
+                $cariDurum = 1;
+            } else {
+                $cariDurum = 0;
+            }
+        } else {
+            $cariDurum = 1;
         }
 
         /**Dosya acılış tarihine göre filtreleme */
@@ -47,7 +57,7 @@ class VisaController extends Controller
             ->leftJoin('visa_file_grades', 'visa_file_grades.id', '=', 'visa_files.visa_file_grades_id')
 
             ->groupBy('visa_files.visa_file_grades_id')
-            ->where('visa_files.active', '=', 1)
+            ->where('visa_files.active', '=', $cariDurum)
 
             ->whereDate('visa_files.created_at', '>=', $startDate)
             ->whereDate('visa_files.created_at', '<=', $endDate)
@@ -62,7 +72,7 @@ class VisaController extends Controller
             ->leftJoin('application_offices', 'application_offices.id', '=', 'visa_files.application_office_id')
 
             ->groupBy('visa_files.application_office_id')
-            ->where('visa_files.active', '=', 1)
+            ->where('visa_files.active', '=', $cariDurum)
 
             ->whereDate('visa_files.created_at', '>=', $startDate)
             ->whereDate('visa_files.created_at', '<=', $endDate)
@@ -70,41 +80,137 @@ class VisaController extends Controller
             ->pluck('total', 'application_office_name')->all();
 
         $arrayVisaFilesAdvisorsAnalist = [];
+        $arrayVisaFilesExpertsAnalist = [];
+        $arrayVisaFilesTranslationsAnalist = [];
 
-        $AllAdvisors = DB::table('users')->select(['id', 'name'])->where('active', '=', 1)
-            ->where('user_type_id', '=',  env('ADVISOR_USER_TYPE_ID'))->get();
+        $allAdvisors = DB::table('users')
+        ->select(['id', 'name'])->where('active', '=', 1)
+        ->where('user_type_id', '=',  env('ADVISOR_USER_TYPE_ID'))
+        ->get();
 
-        foreach ($AllAdvisors as $AllAdvisor) {
+        foreach ($allAdvisors as $allAdvisor) {
+
             $positiveCount =   DB::table('visa_files')
                 ->join('visa_application_result', 'visa_application_result.visa_file_id', '=', 'visa_files.id')
-                ->where('visa_files.advisor_id', '=', $AllAdvisor->id)
+                ->where('visa_files.advisor_id', '=', $allAdvisor->id)
                 ->where('visa_application_result.visa_result', '=', env('VISA_APPLICATION_RESULT_POSITIVE_ID'))
+                ->where('visa_files.active', '=', $cariDurum)
+
                 ->whereDate('visa_files.created_at', '>=', $startDate)
                 ->whereDate('visa_files.created_at', '<=', $endDate)
                 ->get()->count();
             $negativeCount =  DB::table('visa_files')
                 ->join('visa_application_result', 'visa_application_result.visa_file_id', '=', 'visa_files.id')
-                ->where('visa_files.advisor_id', '=', $AllAdvisor->id)
+                ->where('visa_files.advisor_id', '=', $allAdvisor->id)
                 ->where('visa_application_result.visa_result', '=', env('VISA_APPLICATION_RESULT_NEGATIVE_ID'))
+                ->where('visa_files.active', '=', $cariDurum)
+
                 ->whereDate('visa_files.created_at', '>=', $startDate)
                 ->whereDate('visa_files.created_at', '<=', $endDate)
                 ->get()->count();
             $iadeCount =  DB::table('visa_files')
                 ->join('visa_application_result', 'visa_application_result.visa_file_id', '=', 'visa_files.id')
-                ->where('visa_files.advisor_id', '=', $AllAdvisor->id)
+                ->where('visa_files.advisor_id', '=', $allAdvisor->id)
                 ->where('visa_application_result.visa_result', '=', env('VISA_APPLICATION_RESULT_IADE'))
+                ->where('visa_files.active', '=', $cariDurum)
+
                 ->whereDate('visa_files.created_at', '>=', $startDate)
                 ->whereDate('visa_files.created_at', '<=', $endDate)
                 ->get()->count();
-
+            if ($iadeCount == 0 && $negativeCount == 0 && $positiveCount == 0)
+                continue;
             array_push($arrayVisaFilesAdvisorsAnalist, array(
-                $AllAdvisor->name,
+                $allAdvisor->name,
                 $positiveCount == null ? 0 : $positiveCount,
                 $negativeCount == null ? 0 : $negativeCount,
                 $iadeCount == null ? 0 : $iadeCount
             ));
         }
 
+        $allExperts = DB::table('users')
+        ->select(['id', 'name'])
+        ->where('active', '=', 1)
+        ->where('user_type_id', '=',  env('EXPERT_USER_TYPE_ID'))
+        ->get();
+
+        foreach ($allExperts as $allExpert) {
+
+            $positiveCount =   DB::table('visa_files')
+                ->join('visa_application_result', 'visa_application_result.visa_file_id', '=', 'visa_files.id')
+                ->where('visa_files.expert_id', '=', $allExpert->id)
+                ->where('visa_application_result.visa_result', '=', env('VISA_APPLICATION_RESULT_POSITIVE_ID'))
+                ->where('visa_files.active', '=', $cariDurum)
+
+                ->whereDate('visa_files.created_at', '>=', $startDate)
+                ->whereDate('visa_files.created_at', '<=', $endDate)
+                ->get()->count();
+            $negativeCount =  DB::table('visa_files')
+                ->join('visa_application_result', 'visa_application_result.visa_file_id', '=', 'visa_files.id')
+                ->where('visa_files.expert_id', '=', $allExpert->id)
+                ->where('visa_application_result.visa_result', '=', env('VISA_APPLICATION_RESULT_NEGATIVE_ID'))
+                ->where('visa_files.active', '=', $cariDurum)
+
+                ->whereDate('visa_files.created_at', '>=', $startDate)
+                ->whereDate('visa_files.created_at', '<=', $endDate)
+                ->get()->count();
+            $iadeCount =  DB::table('visa_files')
+                ->join('visa_application_result', 'visa_application_result.visa_file_id', '=', 'visa_files.id')
+                ->where('visa_files.expert_id', '=', $allExpert->id)
+                ->where('visa_application_result.visa_result', '=', env('VISA_APPLICATION_RESULT_IADE'))
+                ->where('visa_files.active', '=', $cariDurum)
+
+                ->whereDate('visa_files.created_at', '>=', $startDate)
+                ->whereDate('visa_files.created_at', '<=', $endDate)
+                ->get()->count();
+            if ($iadeCount == 0 && $negativeCount == 0 && $positiveCount == 0)
+                continue;
+            array_push($arrayVisaFilesExpertsAnalist, array(
+                $allExpert->name,
+                $positiveCount == null ? 0 : $positiveCount,
+                $negativeCount == null ? 0 : $negativeCount,
+                $iadeCount == null ? 0 : $iadeCount
+            ));
+        }
+
+        $allTranslations = DB::table('users')
+        ->select(['id', 'name'])
+        ->where('active', '=', 1)
+        ->where('user_type_id', '=',  env('TRANSLATION_USER_TYPE_ID'))
+        ->get();
+
+        $allVisaFileCount =  DB::table('visa_files')
+            ->join('visa_translations', 'visa_translations.visa_file_id', '=', 'visa_files.id')
+            ->where('visa_files.active', '=', $cariDurum)
+            ->whereDate('visa_files.created_at', '>=', $startDate)
+            ->whereDate('visa_files.created_at', '<=', $endDate)
+            ->get()->count();
+
+
+        foreach ($allTranslations as $allTranslation) {
+
+            $visaFilesTranslation = DB::table('visa_files')
+                ->select([
+                    DB::raw("COUNT(visa_files.id) as visa_files_count"),
+                    DB::raw("SUM(visa_translations.translated_page) as translated_page_count"),
+                    DB::raw("SUM(visa_translations.translated_word) as translated_word_count"),
+                ])
+                ->join('visa_translations', 'visa_translations.visa_file_id', '=', 'visa_files.id')
+                ->where('visa_files.translator_id', '=', $allTranslation->id)
+                ->where('visa_files.active', '=', $cariDurum)
+
+                ->whereDate('visa_files.created_at', '>=', $startDate)
+                ->whereDate('visa_files.created_at', '<=', $endDate)
+                ->first();
+
+            if ($visaFilesTranslation->visa_files_count == 0)
+                continue;
+            array_push($arrayVisaFilesTranslationsAnalist, array(
+                $allTranslation->name,
+                $visaFilesTranslation->visa_files_count == null ? 0 : $visaFilesTranslation->visa_files_count,
+                $visaFilesTranslation->translated_page_count == null ? 0 : $visaFilesTranslation->translated_page_count,
+                $visaFilesTranslation->translated_word_count == null ? 0 : $visaFilesTranslation->translated_word_count,
+            ));
+        }
         return view('management.visa.index')->with([
             'countVisaTypes' => $countVisaTypes,
             'countFileGrades' => $countVisaFileGrades,
@@ -115,6 +221,9 @@ class VisaController extends Controller
             'visaFilesGradesCount' => $visaFilesGradesCount,
             'visaFilesApplicationOfficeCount' => $visaFilesApplicationOfficeCount,
             'arrayVisaFilesAdvisorsAnalist' => $arrayVisaFilesAdvisorsAnalist,
+            'arrayVisaFilesExpertsAnalist' => $arrayVisaFilesExpertsAnalist,
+            'arrayVisaFilesTranslationsAnalist' => $arrayVisaFilesTranslationsAnalist,
+            'allVisaFileCount' => $allVisaFileCount,
         ]);
     }
     public function get_danisman(Request $request)
--- resources/views/include/management/visa/chartjs.blade.php
+++ resources/views/include/management/visa/chartjs.blade.php
@@ -3,6 +3,11 @@
         <div class="float-end">
             <form action="/yonetim/vize" method="GET">
                 <div class="input-group input-group-sm">
+                    <span class="input-group-text ">Dosya Tipi:</span>
+                    <select class="form-control" name="status" id="">
+                        <option value="cari" @if (request()->get('status') == 'cari') selected @endif>Cari</option>
+                        <option value="arsiv" @if (request()->get('status') == 'arsiv') selected @endif>Arşiv</option>
+                    </select>
                     <span class="input-group-text ">Tarih Aralığı:</span>
                     <input type="text" name="dates"
                         value="{{ request('dates') ? request('dates') : date('Y-m-01') . '--' . date('Y-m-28') }}"
@@ -14,25 +19,47 @@
     </div>
     <div class="card-body scroll">
         <div class="row">
-            <div class="col-lg-6 ">
-                <label class="text text-dark fw-bold">Aşamalara Göre</label>
-                <div style="width: 100%">
-                    <canvas id="myChart"></canvas>
+            @if ($visaFilesGradesCount != null)
+                <div class="col-lg-6 ">
+                    <label class="text text-dark fw-bold">Aşamalara Göre</label>
+                    <div style="width: 100%">
+                        <canvas id="myChart"></canvas>
+                    </div>
                 </div>
-            </div>
-
-            <div class="col-lg-6">
-                <label class="text text-dark fw-bold">Başvuru Ofislere Göre</label>
-                <div style="width: 100%">
-                    <canvas id="myChart2"></canvas>
+            @endif
+            @if ($visaFilesApplicationOfficeCount != null)
+                <div class="col-lg-6">
+                    <label class="text text-dark fw-bold">Başvuru Ofislere Göre</label>
+                    <div style="width: 100%">
+                        <canvas id="myChart2"></canvas>
+                    </div>
                 </div>
-            </div>
-            <div class="col-lg-6">
-                <label class="text text-dark fw-bold">Danışman Vize Sonuc Performansı(VİZE x RED)</label>
-                <div style="width: 100%">
-                    <canvas id="myChart3"></canvas>
+            @endif
+            @if ($arrayVisaFilesTranslationsAnalist != null)
+                <div class="col-lg-6">
+                    <label class="text text-dark fw-bold">Tercüman Vize Dosyası Tercüme Performansı(Sayfa x
+                        Kelime)</label>
+                    <div style="width: 100%">
+                        <canvas id="myChart5"></canvas>
+                    </div>
                 </div>
-            </div>
+            @endif
+            @if ($arrayVisaFilesAdvisorsAnalist != null)
+                <div class="col-lg-6">
+                    <label class="text text-dark fw-bold">Danışman Vize Sonuc Performansı(VİZE x RED)</label>
+                    <div style="width: 100%">
+                        <canvas id="myChart3"></canvas>
+                    </div>
+                </div>
+            @endif
+            @if ($arrayVisaFilesExpertsAnalist != null)
+                <div class="col-lg-6">
+                    <label class="text text-dark fw-bold">Uzman Vize Sonuc Performansı(VİZE x RED)</label>
+                    <div style="width: 100%">
+                        <canvas id="myChart4"></canvas>
+                    </div>
+                </div>
+            @endif
         </div>
     </div>
 </div>
