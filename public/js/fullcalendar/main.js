70cf35ce2cfe413d673c0a77ba1b2438ec5c3fb2
CSS Legal <rplusmediaweb@gmail.com>
2022-09-26 17:37:50 +0300

    Takvim gösterimi buttonları dark olarak güncellend

--- public/js/fullcalendar/main.js
+++ public/js/fullcalendar/main.js
@@ -4211,7 +4211,7 @@ var FullCalendar = (function (exports) {
         moreLinkText: 'more',
         noEventsText: 'No events to display',
     };
-    var RAW_EN_LOCALE = __assign(__assign({}, MINIMAL_RAW_EN_LOCALE), { 
+    var RAW_EN_LOCALE = __assign(__assign({}, MINIMAL_RAW_EN_LOCALE), {
         // Includes things we don't want other locales to inherit,
         // things that derive from other translatable strings.
         buttonHints: {
@@ -6796,7 +6796,7 @@ var FullCalendar = (function (exports) {
         var endMarker = framingRange.end;
         var instanceStarts = [];
         while (dayMarker < endMarker) {
-            var instanceStart 
+            var instanceStart
             // if everyday, or this particular day-of-week
             = void 0;
             // if everyday, or this particular day-of-week
@@ -13834,7 +13834,7 @@ var FullCalendar = (function (exports) {
             if (!slatCoords) {
                 return null;
             }
-            return segs.map(function (seg, i) { return (createElement(NowIndicatorRoot, { isAxis: false, date: date, 
+            return segs.map(function (seg, i) { return (createElement(NowIndicatorRoot, { isAxis: false, date: date,
                 // key doesn't matter. will only ever be one
                 key: i }, function (rootElRef, classNames, innerElRef, innerContent) { return (createElement("div", { ref: rootElRef, className: ['fc-timegrid-now-indicator-line'].concat(classNames).join(' '), style: { top: slatCoords.computeDateTop(seg.start, date) } }, innerContent)); })); });
         };
@@ -14591,7 +14591,7 @@ var FullCalendar = (function (exports) {
         root: 'fc-theme-bootstrap5',
         tableCellShaded: 'fc-theme-bootstrap5-shaded',
         buttonGroup: 'btn-group',
-        button: 'btn btn-primary',
+        button: 'btn btn-dark',
         buttonActive: 'active',
         popover: 'popover',
         popoverHeader: 'popover-header',
